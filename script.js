ffaaefee316a98ef1bf4088fb1547e5e9ca9fd84
bristricmirza93-a <bristricmirza93@gmail.com>
2026-02-08 05:40:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
-let container = document.getElementById("news");
-let allNews = JSON.parse(localStorage.getItem("news") || "[]");
+let newsBox = document.getElementById("news");
+let leaksBox = document.getElementById("leaks");
 
-function render(news){
-container.innerHTML="";
-news.forEach(n=>{
-container.innerHTML += `
-<div class="card" onclick='openArticle(${JSON.stringify(n)})'>
+let allNews = JSON.parse(localStorage.getItem("news") || []);
 
+function render(){
+newsBox.innerHTML="";
+leaksBox.innerHTML="";
+
+allNews.forEach((n,i)=>{
+
+let card = `
+<div class="card" onclick="openArticle(${i})">
 ${n.image ? `<img src="${n.image}">` : ""}
-
 <h3>${n.title}</h3>
-<p>${n.category || "General"}</p>
+<p>${n.category||"General"}</p>
+<div style="color:#aaa;font-size:12px">👁 ${n.views||0} views</div>
 </div>
 `;
+
+newsBox.innerHTML += card;
+
+if(n.title.toLowerCase().includes("leak")){
+leaksBox.innerHTML += card;
+}
+
 });
 }
 
-render(allNews);
+render();
 
-// SEARCH
-function searchNews(){
-let val=document.getElementById("search").value.toLowerCase();
-let filtered=allNews.filter(n=>n.title.toLowerCase().includes(val));
-render(filtered);
-}
-
-// FILTER
-function filterCat(cat){
-if(cat=="All"){render(allNews);return;}
-let filtered=allNews.filter(n=>n.category==cat);
-render(filtered);
-}
-
-// OPEN ARTICLE
-function openArticle(n){
-localStorage.setItem("openArticle",JSON.stringify(n));
+function openArticle(i){
+allNews[i].views = (allNews[i].views||0)+1;
+localStorage.setItem("news",JSON.stringify(allNews));
+localStorage.setItem("openArticle",JSON.stringify(allNews[i]));
 window.location="article.html";
 }
